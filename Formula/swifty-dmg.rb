f1aed2c115cd66bc5b22bda174e97109af3655d3
Chocoford <504001583@qq.com>
2023-12-26 15:47:37 +0800

    Update swifty-dmg.rb

--- Formula/swifty-dmg.rb
+++ Formula/swifty-dmg.rb
@@ -1,8 +1,8 @@
 class SwiftyDmg < Formula
   desc "Help you create DMG for your app"
   homepage "https://github.com/chocoford/SwiftyDMG"
-  url "https://github.com/chocoford/SwiftyDMG/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "9e36450b84480810428780f42403298e1405909b737e63e4d398691184e8fad5"
+  url "https://github.com/chocoford/SwiftyDMG/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "b3e56b7c1c5154a577c1fc012853734a1690d7b5de8613343973f2232df6d2a6"
   license "MIT"
   head "https://github.com/chocoford/SwiftyDMG.git"
 
